d1fa2580b15238210720f151ef4491215ba3c8c5
Fercho1105 <109244480+Fercho1105@users.noreply.github.com>
2025-06-24 20:05:58 -0600

    Update Constantes.kt

--- Constantes.kt
+++ Constantes.kt
@@ -1,6 +1,6 @@
 package com.example.tripmates
 
 object Constantes {
-    //const val BASE_URL = "http://192.168.0.11/proymov/" //CASA
-    const val BASE_URL = "http://192.168.99.159/proymov/" //CEL
+    //const val BASE_URL = "http://192.168.X.X/proymov/" //CASA
+    const val BASE_URL = "http://192.168.X.X/proymov/" //CEL
 }
